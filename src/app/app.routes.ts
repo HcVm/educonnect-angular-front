0769188d7128bcbe4bcd1b05db0c1bb0c62b97b6
Gvasqueza <gvasquez2511@gmail.com>
2024-07-16 11:16:59 -0500

    a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,21 +4,24 @@ import { RegistroComponent } from './components/registro/registro.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MembresiasComponent } from './components/membresias/membresias.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { AuthGuard } from './guards/auth.guard';
-import { EditarUsuarioComponent } from './components/editar-usuario/editar-usuario.component';
-import { ListarUsuariosComponent } from './components/listar-usuarios/listar-usuarios.component';
+import { AuthGuard } from './guards/auth.guard'; // Asegúrate de tener tu AuthGuard creado
 import { PrincipalPageComponent } from './components/principal-page/principal-page.component';
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
 import { ContactanosComponent } from './components/contactanos/contactanos.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirige a login por defecto
+  { path: '', redirectTo: '', pathMatch: 'full' }, // Redirige a login por defecto
+  { path: '', component: PrincipalPageComponent },  
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegistroComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'contactanos', component: ContactanosComponent },
+
   
   // Rutas protegidas (requieren autenticación)
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'membresias', component: MembresiasComponent, canActivate: [AuthGuard] },
 
+  // Ruta para página no encontrada (404)
   { path: '**', component: NotFoundComponent }
 ];
